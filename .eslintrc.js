11cef3a6bec66fc62beb7c85d470704298c2f322
Ben Remiche <ben.remiche@cgie.lu>
2022-05-03 07:54:03 +0200

    implement eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,23 @@ module.exports = {
   extends: [
     'plugin:vue/essential',
     'eslint:recommended',
+    '@vue/typescript/recommended',
     '@vue/airbnb',
   ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: 'off',
     'linebreak-style': 0,
     'vuejs-accessibility/form-control-has-label': 0,
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'vue/no-unused-components': ['warn', { ignoreWhenBindingPresent: true }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
-<div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <button class="btn">Click me</button>
-</div>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="btn">Click me</button>
+  </div>
 </template>
